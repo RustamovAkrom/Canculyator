c0f8c78a8d6c400d93a0e1d68545e98a7f9b5667
Akromjon <akromjonrustamov56@gmail.com>
2024-05-30 01:37:39 +0500

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-
 from kivy.config import Config
 
 Config.set('graphics', 'resizable', False)
